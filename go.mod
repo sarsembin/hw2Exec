e46cfdc1dd26aeae1d3748fd2a36d3280f089f45
sarsembin <daur2000@gmail.com>
2021-10-24 03:26:20 +0600

    main: RegisterFlip used v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sarsembin/hw2Exec
+
+go 1.17
+
+require github.com/sarsembin/hw2Libs v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sarsembin/hw2Libs"
+)
+
+func main() {
+	fmt.Println(hw2Libs.RegisterFlip("AbObUs"))
+}
